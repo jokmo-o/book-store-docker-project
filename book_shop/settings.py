5d928628586ca71877fc3e0ae7562ccbcfe380ff
Hamza <aldibhamza@gmail.com>
2026-05-20 00:16:46 +0400

    static root change

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/app/staticfiles'
-
+STATIC_ROOT = os.environ.get("STATIC_ROOT", BASE_DIR / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
